fb0d16badf902fa97796ef04894d9bdbcd44382e
Mahlomola Moses <52369928+Mahlomola-Moses@users.noreply.github.com>
2024-12-08 21:40:35 +0200

    create user stracture | module | dto | entity

---
+++ src/db/entities/user.enity.ts
@@ -0,0 +1,23 @@
+/* eslint-disable prettier/prettier */
+import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+
+@Entity('users')
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ type: 'varchar', length: 50 })
+  firstName: string;
+
+  @Column({ type: 'varchar', length: 50 })
+  lastName: string;
+
+  @Column({ type: 'varchar', length: 100, unique: true })
+  email: string;
+
+  @Column({ type: 'varchar', length: 255 })
+  password: string;
+
+  @Column({ type: 'varchar', length: 255 })
+  role: string;
+}
---
+++ src/users/dto/create-user.dto.ts
@@ -0,0 +1,28 @@
+/* eslint-disable prettier/prettier */
+import { ApiProperty } from '@nestjs/swagger';
+import { IsEmail, IsNotEmpty, Length } from 'class-validator';
+
+export class CreateUserDto {
+  @ApiProperty({ description: 'First Name of the user', example: 'John' })
+  @IsNotEmpty()
+  @Length(3, 50)
+  firsttName: string;
+
+  @ApiProperty({ description: 'Last Name of the user', example: 'Doe' })
+  @IsNotEmpty()
+  @Length(3, 50)
+  lastName: string;
+
+  @ApiProperty({ description: 'Email address', example: 'moses@example.com' })
+  @IsEmail()
+  email: string;
+
+  @ApiProperty({
+    description: 'Password for the user',
+    example: 'StrongP@ssw0rd!',
+  })
+  @IsNotEmpty()
+  password: string;
+
+  role: string;
+}
