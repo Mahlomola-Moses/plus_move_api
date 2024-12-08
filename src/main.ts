c6526e81c5df186874022813ccc680eb13ca8c84
Mahlomola Moses <52369928+Mahlomola-Moses@users.noreply.github.com>
2024-12-08 21:18:51 +0200

    initialize swagger api doc

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,25 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import helmet from 'helmet';
+import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.use(helmet());
+
+  app.useGlobalPipes(new ValidationPipe());
+
+  const config = new DocumentBuilder()
+    .setTitle('PlusMove API')
+    .setDescription('API documentation for PlusMove')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('api', app, document);
 
   await app.listen(process.env.PORT ?? 3000);
 }
