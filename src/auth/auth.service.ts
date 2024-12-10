83a57b038a9a8d4007ca0badce3026e41319a68c
Mahlomola Moses <52369928+Mahlomola-Moses@users.noreply.github.com>
2024-12-10 22:13:39 +0200

    validate logging in user and an return token

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,32 @@
+/* eslint-disable prettier/prettier */
 import { Injectable } from '@nestjs/common';
-
+import { JwtService } from '@nestjs/jwt';
+import { UsersService } from '../users/users.service';
+import { JwtPayload } from './interfaces/jwt-payload.interface';
+import * as bcrypt from 'bcrypt';
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    private usersService: UsersService,
+    private jwtService: JwtService,
+  ) {}
+  public async validateUser(id: number, pass: string): Promise<any> {
+    const user = await this.usersService.findOne(id);
+    if (user && (await bcrypt.compare(pass, user.password))) {
+      const { password, ...result } = user;
+      return result;
+    }
+    return null;
+  }
+
+  public async login(user: any) {
+    const payload: JwtPayload = {
+      username: user.username,
+      sub: user.userId,
+      role: user.role,
+    };
+    return {
+      access_token: this.jwtService.sign(payload),
+    };
+  }
+}
