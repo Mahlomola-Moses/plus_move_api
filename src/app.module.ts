75ddae0857488941256fdf7e73cb873b2f95cdb3
Mahlomola Moses <52369928+Mahlomola-Moses@users.noreply.github.com>
2024-12-10 21:14:55 +0200

    restructure auth directory

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,7 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { AuthModule } from './auth/auth/auth.module';
+import { AuthModule } from './auth/auth.module';
 import { DbModule } from './db/entities/db.module';
 import { UsersModule } from './users/users.module';
 
