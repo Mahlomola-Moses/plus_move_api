3467d75ff228789f42e27006fdadc59b8f2eaaf5
Mahlomola Moses <52369928+Mahlomola-Moses@users.noreply.github.com>
2024-12-10 21:39:24 +0200

    build and boilerplate jwt

---
+++ src/auth/strategy/jwt.strategy.ts
@@ -0,0 +1,16 @@
+/* eslint-disable prettier/prettier */
+import { Injectable } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+import { ConfigService } from '@nestjs/config';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(private configService: ConfigService) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: configService.get<string>('JWT_SECRET'),
+    });
+  }
+}
