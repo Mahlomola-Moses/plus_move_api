a7145c7bb788ef0c83f298eb360062bf0fa33bac
Mahlomola Moses <52369928+Mahlomola-Moses@users.noreply.github.com>
2024-12-15 22:53:11 +0200

    Update jwt.strategy.ts

--- src/auth/strategy/jwt.strategy.ts
+++ src/auth/strategy/jwt.strategy.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
 import { ConfigService } from '@nestjs/config';
+import { JwtPayload } from '../interfaces/jwt-payload.interface';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
@@ -12,5 +13,14 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
       ignoreExpiration: false,
       secretOrKey: configService.get<string>('JWT_SECRET'),
     });
+  }
+
+  public async validate(payload: JwtPayload) {
+    return {
+      userId: payload.sub,
+      username: payload.username,
+      email: payload.email,
+      role: payload.role,
+    };
   }
 }
